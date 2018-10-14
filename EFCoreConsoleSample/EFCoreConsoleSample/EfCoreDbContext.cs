c2ab97c19af7e7cb653f59ef3b860bdb0d53fe06
dhyana1984 <xiongyi_1984@foxmail.com>
2018-10-15 00:25:56 +0800

    initial

---
+++ EFCoreConsoleSample/EFCoreConsoleSample/EfCoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreConsoleSample.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace EFCoreConsoleSample
+{
+    public  class EfCoreDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            //ConfigurationManager需要引用System.Configuration.dll
+            optionBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["EFCoreConnectionString"].ConnectionString);
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ EFCoreConsoleSample/EFCoreConsoleSample/Entity/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreConsoleSample.Entity
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public byte Status { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
